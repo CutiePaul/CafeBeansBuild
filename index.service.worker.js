14646d4c7471687055529d2071e5365d7184df47
Paul <paul.riesch@t-online.de>
2026-01-19 22:14:24 +0100

    add credits

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1768842121|2561660485';
+const CACHE_VERSION = '1768857234|17672584226';
 /** @type {string} */
 const CACHE_PREFIX = 'CafeBeans-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
